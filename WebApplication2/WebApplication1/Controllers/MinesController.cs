66f1b4f25e7454c171c97563d8e0ecc8ef2488c0
Cristina Volinteriu <cristinavolinteriu@gmail.com>
2017-04-11 11:09:20 -0700

    Lab2

---
+++ WebApplication2/WebApplication1/Controllers/MinesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    public class MinesController : Controller
+    {
+        // GET: Default
+        ApplicationDbContext dbContext = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            var userId = this.User.Identity.GetUserId();
+            var user = dbContext.Users.Find(userId);
+            var city = user.Cities.First();
+            return View(city);
+        }
+
+        public ActionResult Details(int mineId)
+        {
+            var mine = dbContext.Mines.Find(mineId);
+            return View(mine);
+        }
+    }
+}
